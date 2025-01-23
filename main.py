ddc3904018e5e9f304337a79794bdf6fe74f6373
kavan6 <kavanheppenstall@gmail.com>
2025-01-23 11:11:13 +0000

    Added show labelling mode

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 ### Product labelling software for excel spreadsheets
 # Use "pyinstaller --onefile --add-data "Helvetica.ttf;." --add-data "FRE3OF9X.ttf;." main.py" to build
 
+import threading
+import sys
 import os
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, ttk
 import xlwings as xw
 import fpdf as fpdf
 
@@ -21,33 +23,64 @@ def UploadAction(event=None):
     if(bookName):
         messagebox.showinfo("File Uploaded", f"Spreadsheet loaded: {bookName}")
 
-def create_label(product_name, product_price, product_SKU, product_barcode):
-    curr_font = 28
-
+def create_label(product_name, product_price, product_SKU, product_barcode, isShow = False):
     pdf = fpdf.FPDF(orientation="L", unit="mm", format="A4")
     pdf.add_page()
     pdf.set_margins(3, 2.5, 3)
     pdf.b_margin = 2.5
-    pdf.add_font('barcode', style='', fname=barcode_path)
-    pdf.add_font('helvet', style='', fname=helvet_path)
+
+    with open(os.devnull, 'w') as devnull:
+        old_stderr = sys.stderr
+        sys.stderr = devnull
+        try:
+            pdf.add_font('barcode', style='', fname=barcode_path)
+            pdf.add_font('helvet', style='', fname=helvet_path)
+        finally:
+            sys.stderr = old_stderr
+
+    curr_font = 28
     pdf.set_font('helvet', size=curr_font)
 
     page_width = pdf.w - pdf.l_margin - pdf.r_margin
 
-    pname_width = pdf.get_string_width(product_name) + 10
-    pprice_width = pdf.get_string_width(product_price)
-
-    while(pname_width + pprice_width > page_width):
-        curr_font -= 1
-        pdf.set_font('helvet', size=curr_font)
-
+    if(not isShow):
         pname_width = pdf.get_string_width(product_name) + 10
         pprice_width = pdf.get_string_width(product_price)
 
-    pdf.set_x(pdf.l_margin)
-    pdf.cell(pname_width, 85, product_name, align="L")
-    pdf.set_x(pdf.w - pdf.r_margin - pprice_width)
-    pdf.cell(pprice_width, 85, product_price, align="R")
+        while(pname_width + pprice_width > page_width):
+            curr_font -= 1
+            pdf.set_font('helvet', size=curr_font)
+
+            pname_width = pdf.get_string_width(product_name) + 10
+            pprice_width = pdf.get_string_width(product_price)
+
+        pdf.set_x(pdf.l_margin)
+        pdf.cell(pname_width, 85, product_name, align="L")
+        pdf.set_x(pdf.w - pdf.r_margin - pprice_width)
+        pdf.cell(pprice_width, 85, product_price, align="R")
+    else:
+        curr_font_SKU = 44
+        pdf.set_font('helvet', size=curr_font_SKU)
+        SKU_width = pdf.get_string_width(product_SKU) + 10
+        curr_font = 72
+        pdf.set_font('helvet', size=curr_font)
+        pprice_width = pdf.get_string_width(product_price)
+
+        while(SKU_width + pprice_width > page_width):
+            curr_font_SKU -= 1
+            pdf.set_font('helvet', size=curr_font_SKU)
+            SKU_width = pdf.get_string_width(product_SKU) + 10
+
+            curr_font -= 1
+            pdf.set_font('helvet', size=curr_font)
+            pprice_width = pdf.get_string_width(product_price)
+
+        pdf.set_x(pdf.l_margin)
+        pdf.set_font('helvet', size=curr_font_SKU)
+        pdf.cell(SKU_width, 85, product_SKU, align="L")
+        pdf.set_x(pdf.w - pdf.r_margin - pprice_width)
+        pdf.set_font('helvet', size=curr_font)
+        pdf.cell(pprice_width, 85, product_price, align="R")
 
     curr_font = 156
     pdf.set_font('barcode', size=curr_font)
@@ -62,66 +95,110 @@ def create_label(product_name, product_price, product_SKU, product_barcode):
     pdf.set_y(90)
     pdf.cell(page_width, 9, product_barcode, align="C")
 
-    curr_font = 56
-    pdf.set_font('helvet', size=curr_font)
-    SKU_width = pdf.get_string_width(product_SKU)
-
-    while(SKU_width > page_width):
-        curr_font -= 1
-        pdf.set_font('helvet', size=56)
-        
+    if(not isShow):
+        curr_font = 56
+        pdf.set_font('helvet', size=curr_font)
         SKU_width = pdf.get_string_width(product_SKU)
 
-    pdf.set_y(130)
-    pdf.cell(page_width, 9, product_SKU, align="C")
+        while(SKU_width > page_width):
+            curr_font -= 1
+            pdf.set_font('helvet', size=curr_font)
+            
+            SKU_width = pdf.get_string_width(product_SKU)
 
-    #product_SKU = product_SKU.replace("/", "_").replace("\\", "_")
+        pdf.set_y(130)
+        pdf.cell(page_width, 9, product_SKU, align="C")
+    else:
+        curr_font = 32
+        pdf.set_font('helvet', size=curr_font)
+        pname_width = pdf.get_string_width(product_name)
 
-    os.makedirs("labels", exist_ok=True)
-    pdf.output(f"labels/{product_SKU}.pdf")
+        while(pname_width > page_width):
+            curr_font -= 1
+            pdf.set_font('helvet', size=curr_font)
+            
+            pname_width = pdf.get_string_width(product_name)
 
-def CreateLabels(event=None):
-    global bookName
-    if not bookName:
-        messagebox.showwarning("No file", "Please upload a spreadsheet first")
-        return
-    
-    try:
-        wb = xw.Book(bookName) 
-        sheet = wb.sheets[0]
+        pdf.set_y(130)
+        pdf.cell(page_width, 9, product_name, align="C")
 
-        row = 3
-        while True:
+    if(not isShow):
+        os.makedirs("Warehouse Labels", exist_ok=True)
+        pdf.output(f"Warehouse Labels/{product_SKU}.pdf")
+    else:
+        os.makedirs("Show Labels", exist_ok=True)
+        pdf.output(f"Show Labels/{product_SKU}.pdf")
 
-            product_name = sheet[f"B{row}"].value
-            product_price = sheet[f"C{row}"].value
-            product_SKU = sheet[f"D{row}"].value
-            product_barcode = sheet[f"E{row}"].value
+def CreateLabels(event=None, isShow=False):
+    def task():
+        global bookName
+        if not bookName:
+            messagebox.showwarning("No file", "Please upload a spreadsheet first")
+            return
+        
+        try:
+            wb = xw.Book(bookName) 
+            sheet = wb.sheets[0]
 
-            if not product_name:
-                break
+            row = 3
+            loading_bar.start()
+            while True:
 
-            create_label(
-                str(product_name), 
-                f"£{'{:.2f}'.format(product_price)}" if product_price else "£0.00", 
-                str(product_SKU) if product_SKU else "", 
-                f"*{product_barcode}*" if product_barcode else ""
-            )
-            row += 1
-    except Exception as e:
-        messagebox.showerror("Error", f"An error occurred: {e}")
+                product_name = sheet[f"B{row}"].value
+                product_price = sheet[f"C{row}"].value
+                product_SKU = sheet[f"D{row}"].value
+                product_barcode = sheet[f"E{row}"].value
 
-    messagebox.showinfo("Success", f"Labels successfully created: see labels folder")
-    root.destroy()
+                if not product_name:
+                    break
+
+                create_label(
+                    str(product_name), 
+                    f"£{'{:.2f}'.format(product_price)}" if product_price else "£0.00", 
+                    str(product_SKU) if product_SKU else "", 
+                    f"*{product_barcode}*" if product_barcode else "",
+                    isShow=isShow
+                )
+                row += 1
+
+                root.update_idletasks()
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {e}")
+        finally:
+            loading_bar.stop()
+            root.update_idletasks()
+
+        messagebox.showinfo("Success", f"Labels successfully created: see labels folder")
+
+    loading_bar.start()
+    threading.Thread(target=task).start()
 
 root = tk.Tk()
 root.title("Product Labelling")
 
-upload_button = tk.Button(root, text="Upload Spreadsheet", command=UploadAction)
-upload_button.pack(pady=10, padx=10)
+root.minsize(width=400, height=150)
 
-create_button = tk.Button(root, text="Create Labels", command=CreateLabels)
-create_button.pack(pady=10, padx=10)
+root.grid_columnconfigure(0, weight=1, minsize=40)
+root.grid_columnconfigure(1, weight=2, minsize=80)
+
+
+upload_button = tk.Button(root, text="Upload Spreadsheet", command=UploadAction)
+upload_button.grid(row=0, column=0, pady=10, padx=10, sticky="ew")
+
+show_labels_button = tk.Button(root, text="Create Show Labels", command=lambda: CreateLabels(isShow=True))
+show_labels_button.grid(row=1, column=0, pady=10, padx=10, sticky="ew")
+
+warehouse_labels_button = tk.Button(root, text="Create Warehouse Labels", command=lambda: CreateLabels(isShow=False))
+warehouse_labels_button.grid(row=2, column=0, pady=10, padx=10, sticky="ew")
+
+loading_bar = ttk.Progressbar(root, mode="indeterminate", length=75)
+loading_bar.grid(row=1, column=1, rowspan=1, pady=10, padx=10)
+
+root.update_idletasks()
+button_width = max(upload_button.winfo_width(), show_labels_button.winfo_width(), warehouse_labels_button.winfo_width())
+upload_button.config(width=button_width)
+show_labels_button.config(width=button_width)
+warehouse_labels_button.config(width=button_width)
 
 root.mainloop()
 
